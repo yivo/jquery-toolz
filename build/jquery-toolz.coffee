d6164f55431121b5de2b94b4baba6c48891f3222
Yaroslav Konoplov <eahome00@gmail.com>
2017-01-10 16:07:01 +0200

    1.0.1 build

---
+++ build/jquery-toolz.coffee
@@ -0,0 +1,101 @@
+((factory) ->
+
+  # Browser and WebWorker
+  root = if typeof self is 'object' and self isnt null and self.self is self
+    self
+
+  # Server
+  else if typeof global is 'object' and global isnt null and global.global is global
+    global
+
+  # AMD
+  if typeof define is 'function' and typeof define.amd is 'object' and define.amd isnt null
+    define ['jquery', 'exports'], ($) ->
+      factory(root, document, window, RegExp, $)
+
+  # CommonJS
+  else if typeof module is 'object' and module isnt null and
+          typeof module.exports is 'object' and module.exports isnt null
+    factory(root, document, window, RegExp, require('jquery'))
+
+  # Browser and the rest
+  else
+    factory(root, document, window, RegExp, root.$)
+
+  # No return value
+  return
+
+)((__root__, document, window, RegExp, $) ->
+  # Usage: 
+  #   * $el.removeClass(matching: 'example-*')
+  #   * $el.removeClass(matching: /something/)
+  do ->
+    removeClass = $.fn.removeClass
+    maskCache   = {}
+    maskConvert = (mask) ->
+      return mask if mask instanceof RegExp
+      maskCache[mask] ?= new RegExp(mask.replace(/\*/g, '\\S+'))
+      
+    $.fn.removeClass = (arg) ->
+      if arguments.length > 0
+        if arg isnt null and typeof arg is 'object' and arg.matching?
+          removeClassByMask(this, arg.matching)
+        else
+          removeClass.call(this, arg)
+      else
+        removeClass.call(this)
+          
+    removeClassByMask =
+      if DOMTokenList?
+        (elements, mask) ->
+          re = maskConvert(mask)
+          for el in elements
+            ref = el.classList
+            l   = ref.length
+            i   = -1
+            while ++i < l
+              cls = ref[i]
+              if re.test(cls)
+                el.classList.remove(cls)
+                --l
+                --i
+          elements
+      else
+        (elements, mask) ->
+          re = maskConvert(mask)
+          for el in elements
+            oldlist = el.className.split(' ')
+            newlist = []
+            for cls in oldlist when not re.test(cls)
+              newlist.push(cls)
+            el.className = newlist.join(' ')
+          elements
+  
+  {getComputedStyle} = window
+  
+  css = if getComputedStyle?
+    (el, style) -> getComputedStyle(el)[style]
+  else
+    (el, style) -> $(el).css(style)
+  
+  $.fn.snapshotStyles ?= (styles) ->
+    for el in this
+      for style in styles
+        el.style[style] = css(el, style)
+    this
+  
+  $.fn.releaseSnapshot ?= (styles) ->
+    for el in this
+      el.offsetHeight
+      for style in styles
+        el.style[style] = ''
+    this
+  
+  $.prefix ?= (property) ->
+    ["-moz-#{property}", "-o-#{property}", "-ms-#{property}", "-webkit-#{property}", property]
+  
+  $.fn.tag ?= ->
+    this[0]?.tagName.toLowerCase()
+  # No global variable export
+  return
+)
---
+++ build/jquery-toolz.js
@@ -0,0 +1,121 @@
+(function() {
+  (function(factory) {
+    var root;
+    root = typeof self === 'object' && self !== null && self.self === self ? self : typeof global === 'object' && global !== null && global.global === global ? global : void 0;
+    if (typeof define === 'function' && typeof define.amd === 'object' && define.amd !== null) {
+      define(['jquery', 'exports'], function($) {
+        return factory(root, document, window, RegExp, $);
+      });
+    } else if (typeof module === 'object' && module !== null && typeof module.exports === 'object' && module.exports !== null) {
+      factory(root, document, window, RegExp, require('jquery'));
+    } else {
+      factory(root, document, window, RegExp, root.$);
+    }
+  })(function(__root__, document, window, RegExp, $) {
+    var base, base1, base2, css, getComputedStyle;
+    (function() {
+      var maskCache, maskConvert, removeClass, removeClassByMask;
+      removeClass = $.fn.removeClass;
+      maskCache = {};
+      maskConvert = function(mask) {
+        if (mask instanceof RegExp) {
+          return mask;
+        }
+        return maskCache[mask] != null ? maskCache[mask] : maskCache[mask] = new RegExp(mask.replace(/\*/g, '\\S+'));
+      };
+      $.fn.removeClass = function(arg) {
+        if (arguments.length > 0) {
+          if (arg !== null && typeof arg === 'object' && (arg.matching != null)) {
+            return removeClassByMask(this, arg.matching);
+          } else {
+            return removeClass.call(this, arg);
+          }
+        } else {
+          return removeClass.call(this);
+        }
+      };
+      return removeClassByMask = typeof DOMTokenList !== "undefined" && DOMTokenList !== null ? function(elements, mask) {
+        var cls, el, i, j, l, len, re, ref;
+        re = maskConvert(mask);
+        for (j = 0, len = elements.length; j < len; j++) {
+          el = elements[j];
+          ref = el.classList;
+          l = ref.length;
+          i = -1;
+          while (++i < l) {
+            cls = ref[i];
+            if (re.test(cls)) {
+              el.classList.remove(cls);
+              --l;
+              --i;
+            }
+          }
+        }
+        return elements;
+      } : function(elements, mask) {
+        var cls, el, j, k, len, len1, newlist, oldlist, re;
+        re = maskConvert(mask);
+        for (j = 0, len = elements.length; j < len; j++) {
+          el = elements[j];
+          oldlist = el.className.split(' ');
+          newlist = [];
+          for (k = 0, len1 = oldlist.length; k < len1; k++) {
+            cls = oldlist[k];
+            if (!re.test(cls)) {
+              newlist.push(cls);
+            }
+          }
+          el.className = newlist.join(' ');
+        }
+        return elements;
+      };
+    })();
+    getComputedStyle = window.getComputedStyle;
+    css = getComputedStyle != null ? function(el, style) {
+      return getComputedStyle(el)[style];
+    } : function(el, style) {
+      return $(el).css(style);
+    };
+    if ((base = $.fn).snapshotStyles == null) {
+      base.snapshotStyles = function(styles) {
+        var el, j, k, len, len1, ref1, style;
+        ref1 = this;
+        for (j = 0, len = ref1.length; j < len; j++) {
+          el = ref1[j];
+          for (k = 0, len1 = styles.length; k < len1; k++) {
+            style = styles[k];
+            el.style[style] = css(el, style);
+          }
+        }
+        return this;
+      };
+    }
+    if ((base1 = $.fn).releaseSnapshot == null) {
+      base1.releaseSnapshot = function(styles) {
+        var el, j, k, len, len1, ref1, style;
+        ref1 = this;
+        for (j = 0, len = ref1.length; j < len; j++) {
+          el = ref1[j];
+          el.offsetHeight;
+          for (k = 0, len1 = styles.length; k < len1; k++) {
+            style = styles[k];
+            el.style[style] = '';
+          }
+        }
+        return this;
+      };
+    }
+    if ($.prefix == null) {
+      $.prefix = function(property) {
+        return ["-moz-" + property, "-o-" + property, "-ms-" + property, "-webkit-" + property, property];
+      };
+    }
+    if ((base2 = $.fn).tag == null) {
+      base2.tag = function() {
+        var ref1;
+        return (ref1 = this[0]) != null ? ref1.tagName.toLowerCase() : void 0;
+      };
+    }
+  });
+
+}).call(this);
